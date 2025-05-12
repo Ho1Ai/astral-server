9f9e822a31a4c1f57170d83ac3de3153dfb984b5
Ho1Ai desktop <koekto33309@gmail.com>
2025-05-12 00:14:09 +0000

    added db config

---
+++ db_config.sql
@@ -0,0 +1,29 @@
+create table astraldb_projects (
+	id SERIAL PRIMARY KEY,
+	name varchar(255),
+	description text,
+	author_team varchar(255),
+	authors_list varchar(255)[],
+	project_links_array JSONB
+);
+
+create table astraldb_tdl (
+	id BIGSERIAL PRIMARY KEY,
+	name text,
+	state smallint
+);
+
+create table astraldb_users (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(255) not null,
+	nickname VARCHAR(255) not null,
+	passwd text not null,
+	description text,
+	joined_at TIMESTAMP default CURRENT_TIMESTAMP
+);
+
+create table astraldb_projectusers (
+	id BIGSERIAL primary key,
+	user_id INTEGER references astraldb-users(id),
+	projects_id integer references astraldb-projects(id)
+)
